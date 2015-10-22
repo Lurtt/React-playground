7ebf049cb8ed35f08f695bd6d8fc9aa775ca9b1b
Lurtt <lurttik@gmail.com>
2015-10-22 22:09:44 +0200

    webpack setup #1

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: ["./utils.js", "./app.js"],
+    output: {
+        filename: "bundle.js"
+    },
+
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: 'node_modules',
+                loader: 'jshint-loader'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['', '.js']
+    }
+}
